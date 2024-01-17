412d060771b673ea4cdf7bd196f8881674f6999b
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-17 10:45:08 +0530

    Add files via upload

---
+++ NumberTriangle.java
@@ -0,0 +1,16 @@
+package com.logic.programs;
+
+public class NumberTriangle {
+public static void main(String[] args) {
+	for(int i=1;i<=4;i++)
+	{
+		for(int j=i;j<=3;j++) {
+			System.out.print(" ");
+		}
+		for(int k=1;k<=i;k++)
+			System.out.print(" "+i+" ");
+		System.out.println();
+	}
+	
+}
+}
