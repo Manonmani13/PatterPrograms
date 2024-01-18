aa09b9a7e89ad21e984c345bcee0cfaba7523f01
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-18 09:36:05 +0530

    Add files via upload

---
+++ ReverseLeftPyramid.java
@@ -0,0 +1,16 @@
+package com.logic.programs;
+
+public class ReverseLeftPyramid {
+	public static void main(String[] args) {
+		for(int i=5;i>=1;i--) {
+			for(int j=i;j<5;j++) {
+				System.out.print("   ");
+
+			}
+			for(int j=i;j>=1;j--) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+	}
+}
