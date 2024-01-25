e3c3ae0ae61d3b1d28f40f9f4011a1e86d051833
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-25 14:05:52 +0530

    Add files via upload

--- ReversePyramid.java
+++ ReversePyramid.java
@@ -1,15 +1,16 @@
-package com.logic.programs;
+package patter;
 
 public class ReversePyramid {
-	public static void main(String[] args) {
-		for(int i=4;i>=1;i--)
+public static void main(String[] args) {
+	for(int i=5;i>=1;i--) {
+		for(int j=i;j<5;j++)
 		{
-			
-			for(int k=1;k<=i;k++)
-				System.out.print(k);
-			
-			System.out.println();
+			System.out.print(" ");
 		}
-		
+		for(int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
 	}
+}
 }
