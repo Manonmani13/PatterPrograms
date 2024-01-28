a05034b54a86475c95f4b5f7d04068894f041fe1
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-28 19:19:28 +0530

    Add files via upload

--- NumberPAtter1.java
+++ NumberPAtter1.java
@@ -2,10 +2,16 @@ package com.logic;
 
 public class NumberPAtter1 {
 public static void main(String[] args) {
+	int m=1;
 	for(int i=1;i<=7;i++){
-		for(int j=1;j<=i;j++) {
+		for(int k=i;k<=7;k++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=m;j++) {
 			System.out.print(j);
 		}
+		m++;
 		System.out.println();
 
 	}
