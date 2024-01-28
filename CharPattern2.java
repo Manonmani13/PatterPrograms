e03c62ea580869565527f5cf076812f7d9be150e
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-28 19:04:56 +0530

    Add files via upload

---
+++ CharPattern2.java
@@ -0,0 +1,12 @@
+package com.logic;
+
+public class CharPattern2 {
+	public static void main(String[] args) {
+		for(int i=7;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print((char)(j+64));
+			}
+			System.out.println();
+		}
+	}
+}
