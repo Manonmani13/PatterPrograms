399cbabb419ad88daadf28550a4ed3339ed1a6ee
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-28 19:19:46 +0530

    Add files via upload

--- NumberPattern2.java
+++ NumberPattern2.java
@@ -2,7 +2,12 @@ package com.logic;
 
 public class NumberPattern2 {
 public static void main(String[] args) {
+	int m=1;
 	for(int i=7;i>=1;i--){
+		for(int k=i;k<=7;k++)
+		{
+			System.out.print(" ");
+		}
 		for(int j=1;j<=i;j++) {
 			System.out.print(j);
 		}
