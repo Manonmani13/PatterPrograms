395cc25f179b7129901bc1b0b09ad396354bb471
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-29 20:11:26 +0530

    Add files via upload

---
+++ NumberPatterJREverse.java
@@ -0,0 +1,15 @@
+package patter;
+
+public class NumberPatterJREverse {
+	public static void main(String[] args) {
+		for(int i=7;i>=1;i--) {
+			for(int j=i;j<=7;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print(k);
+			}
+			System.out.println();
+		}
+	}
+}
