7ec3ecc3bea41614b40741f8c7b00693c96ea73e
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-29 20:13:21 +0530

    Add files via upload

---
+++ StarPatterReverse2.java
@@ -0,0 +1,16 @@
+package patter;
+
+public class StarPatterReverse2 {
+	public static void main(String[] args) {
+		for(int i=7;i>=1;i--)
+		{
+			for(int j=i;j<=7;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
