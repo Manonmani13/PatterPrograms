d903a765da6c89aeb6b59907635253837aec1186
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-30 10:51:38 +0530

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,26 @@
+package patter;
+
+import java.util.Iterator;
+
+public class StarPattern {
+public static void main(String[] args) {
+	for(int i=7;i>=1;i--) {
+		for(int j=i;j<=7;j++) {
+			System.out.print("  ");
+		}
+		for(int j=1;j<=i;j++) {
+			System.out.print(" * ");
+		}
+		System.out.println();
+	}
+	for(int i=2;i<=7;i++) {
+		for(int j=i;j<=7;j++) {
+			System.out.print("  ");
+		}
+		for(int j=1;j<=i;j++) {
+			System.out.print(" * ");
+		}
+		System.out.println();
+	}
+}
+}
