d4bc757826a4d0255cf518e0bc431aa5ee9a692c
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-30 10:16:36 +0530

    Add files via upload

---
+++ CharacterPatternLeftReverse.java
@@ -0,0 +1,15 @@
+package patter;
+
+public class CharacterPatternLeftReverse {
+	public static void main(String[] args) {
+		for(int i=7;i>=1;i--) {
+			for(int j=i;j<=7;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print((char)(j+64));
+			}
+			System.out.println();
+		}
+	}
+}
