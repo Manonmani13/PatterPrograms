851a2c9f9d99198275d7761f4951380a57d4084e
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-30 10:15:20 +0530

    Add files via upload

---
+++ CharacterPatternLeft7.java
@@ -0,0 +1,15 @@
+package patter;
+
+public class CharacterPatternLeft7 {
+public static void main(String[] args) {
+	for(int i=1;i<=7;i++) {
+		for(int j=i;j<=7;j++) {
+			System.out.print(" ");
+		}
+		for(int j=1;j<=i;j++) {
+			System.out.print((char)(j+64));
+		}
+		System.out.println();
+	}
+}
+}
