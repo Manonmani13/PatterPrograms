7dd68ddd91ee1d38e89c37c953a6d636dec2e49e
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-31 10:24:13 +0530

    Add files via upload

---
+++ TimeUsingCharacter.java
@@ -0,0 +1,25 @@
+package patter;
+
+public class TimeUsingCharacter {
+	public static void main(String[] args) {
+		for(int i=7;i>=1;i--) {
+			for(int j=i;j<=7;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print(" "+(char)(j+64)+" ");
+			}
+			System.out.println();
+		}
+		
+		for(int i=2;i<=7;i++) {
+			for(int j=i;j<=7;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print(" "+(char)(j+64)+" ");
+			}
+			System.out.println();
+		}
+	}
+}
