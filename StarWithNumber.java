14aac2810fb05c68521caf1f8c2cae2e5984faf4
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-07 13:49:27 +0530

    Add files via upload

---
+++ StarWithNumber.java
@@ -0,0 +1,18 @@
+package patter;
+
+public class StarWithNumber {
+	public static void main(String[] args) {
+		int k=1;
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=7;j++) {
+				if(j%2==0)
+					System.out.print("*");
+				else
+					System.out.print(k);
+			}
+			k++;
+
+			System.out.println();
+		}
+	}
+}
