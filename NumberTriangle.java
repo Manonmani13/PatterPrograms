fb7020542492543043392134a9968a78a3eadbe1
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-08 15:25:57 +0530

    Add files via upload

--- NumberTriangle.java
+++ NumberTriangle.java
@@ -1,16 +1,17 @@
-package com.logic.programs;
+package numberpattern;
 
 public class NumberTriangle {
 public static void main(String[] args) {
-	for(int i=1;i<=4;i++)
-	{
-		for(int j=i;j<=3;j++) {
+	int m=1;
+	for(int i=1;i<=5;i++) {
+		for(int j=i;j<=4;j++) {
 			System.out.print(" ");
 		}
-		for(int k=1;k<=i;k++)
-			System.out.print(" "+i+" ");
+		for(int j=1;j<=m;j++) {
+			System.out.print(i);
+		}
+		m+=2;
 		System.out.println();
 	}
-	
 }
 }
