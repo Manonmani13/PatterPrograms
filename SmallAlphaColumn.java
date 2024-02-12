dbba7e78fc299db0f5b86efec429561c882e2793
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-12 10:48:49 +0530

    Add files via upload

---
+++ SmallAlphaColumn.java
@@ -0,0 +1,12 @@
+package characters;
+
+public class SmallAlphaColumn {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=5;j++) {
+			System.out.print((char)(j+96));
+		}
+		System.out.println();
+	}
+}
+}
