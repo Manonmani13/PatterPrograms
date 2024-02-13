324a1f6093ce880839440401fd79cff35c51ad57
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-13 14:57:35 +0530

    Add files via upload

--- Diamond.java
+++ Diamond.java
@@ -1,29 +1,38 @@
-package com.logic.programs;
+package patter;
 
 public class Diamond {
-	public static void main(String[] args) {
-		for(int i=1;i<=4;i++)
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=i;j<=4;j++)
 		{
-			
-			for(int j=i;j<=3;j++) {
-					System.out.print(" ");
-			}
-			for(int j=0;j<i;j++) {
-				System.out.print(" * ");
-			}
-			
-			System.out.println();
+			System.out.print(" ");
 		}
-		for(int i=1;i<=3;i++)
+		for(int k=1;k<=i;k++)
 		{
-			for(int j=1;j<=i;j++) {
-				System.out.print(" ");
-			}
-			for(int j=i;j<=3;j++) {
-				System.out.print(" * ");
-			}
-			
-			System.out.println();
+			System.out.print("*");
 		}
+		for(int k=1;k<i;k++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
 	}
+	for(int i=4;i>=1;i--)
+	{
+		for(int j=i;j<=4;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int k=1;k<=i;k++)
+		{
+			System.out.print("*");
+		}
+		for(int k=1;k<i;k++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
 }
