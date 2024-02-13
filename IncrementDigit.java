3227789bec48f80e3e5ceece1eec86cdbb26e5ff
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-13 10:17:33 +0530

    Add files via upload

--- IncrementDigit.java
+++ IncrementDigit.java
@@ -2,13 +2,16 @@ package patter;
 
 public class IncrementDigit {
 public static void main(String[] args) {
-	int n=3453,s=0,a=0;
+	int n=4567,s=0,a=0;
+	StringBuffer string=new StringBuffer();
 	while(n>0)
 	{
 		s=(n%10)+1;
 		a=a*10+s;	
 		n=n/10;
 	}
-	System.out.println(" result "+a);
+
+	string.append(String.valueOf(a)).reverse();
+	System.out.println(" result "+string);
 }
 }
