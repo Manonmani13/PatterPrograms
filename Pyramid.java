cd23ca4bfad72ac02f5e9828681e143a4a232b19
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-15 19:39:00 +0530

    Add files via upload

--- Pyramid.java
+++ Pyramid.java
@@ -1,18 +1,17 @@
-package patter;
+package numberpattern;
 
 public class Pyramid {
-	public static void main(String[] args) {
-		for(int i=5;i>=1;i--)
+public static void main(String[] args) {
+	for(int i=1;i<=10;i++)
+	{
+		for(int j=i;j<10;j++)
 		{
-			for(int j=1;j<i;j++)
-			{
-				System.out.print(" ");
-			}
-				for(int j=i;j<=5;j++)
-				{
-					System.out.print("*");
-				}
-				System.out.println();
+			System.out.print(" ");
 		}
+		for(int j=1;j<=i;j++) {
+			System.out.print(j+" ");
+		}
+			System.out.println();
 	}
+}
 }
