eb0e32dc85d281acbe6216fa0cb9f4f20d39f8bf
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-16 19:46:37 +0530

    Add files via upload

---
+++ CheckPrime.java
@@ -0,0 +1,12 @@
+package numberpattern;
+
+public class CheckPrime {
+public static void main(String[] args) {
+	int n=2;
+	
+	if(n%2==0)
+		System.out.println("prime");
+	else
+		System.out.println("not prime");
+}
+}
