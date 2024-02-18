2e3ce47019502ea02abdc2b7e0599861bdd32eff
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-18 22:03:49 +0530

    Add files via upload

--- Pattern1.java
+++ Pattern1.java
@@ -1,10 +1,17 @@
-package patter;
+package com.logic;
 
 public class Pattern1 {
 public static void main(String[] args) {
-	for(int i=1;i<=7;i++) {
-		for(int j=1;j<=i;j++) {
-			System.out.print(i);
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=1;j<=7;j++)
+		{
+			if(i==1||i==7)
+				System.out.print("*");
+			else if(j==1||j==5)
+				System.out.print("*");
+			else
+				System.out.print(" ");
 		}
 		System.out.println();
 	}
