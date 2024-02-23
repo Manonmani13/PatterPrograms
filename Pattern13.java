3bcffd4af00c05b2e600d3266b97077ec216f604
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-23 11:32:43 +0530

    Add files via upload

--- Pattern13.java
+++ Pattern13.java
@@ -2,15 +2,17 @@ package patternapp;
 
 public class Pattern13 {
 	public static void main(String[] args) {
-		int x=1;
+		int x=1,n=5,y=0;
 		for(int i=1;i<=5;i++) {
 			x=i;
+			y=n-i+1;
 			for(int j=1;j<=5;j++) {
-				System.out.print(x+" ");
 				if(j%2==1)
-				x=x+9;
+				System.out.print(x+" ");
 				else
-					x=x+1;
+					System.out.print(y+" ");
+				x=x+n;
+					y=y+n;
 			}
 			System.out.println();
 		}
