6c4e230697d8778d690f56a551f02596824ec80c
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-26 11:43:26 +0530

    Add files via upload

---
+++ Pattern26.java
@@ -0,0 +1,14 @@
+package patternapp;
+
+public class Pattern26 {
+public static void main(String[] args) {
+	for(int i=0;i<5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print((char) (i+65));
+		}
+		System.out.println();
+	}
+}
+}
