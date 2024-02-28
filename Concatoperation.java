21cc4540f3e401d7f734dba93de69bb16fccf240
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-28 17:30:29 +0530

    Add files via upload

--- Concatoperation.java
+++ Concatoperation.java
@@ -3,7 +3,7 @@ package strings;
 public class Concatoperation {
 public static void main(String[] args) {
 	String s1=new String("welcome to");
-	s1.concat("java");
+	s1=s1.concat("java");
 	System.out.println(s1);
 }
 }
