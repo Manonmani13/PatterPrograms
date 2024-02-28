0b32b4027b5d7b2ab86b607b26c10ea3b4c02974
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-28 17:29:24 +0530

    Add files via upload

---
+++ Concatoperation.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class Concatoperation {
+public static void main(String[] args) {
+	String s1=new String("welcome to");
+	s1.concat("java");
+	System.out.println(s1);
+}
+}
