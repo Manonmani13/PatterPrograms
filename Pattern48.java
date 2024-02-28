eda8fedd038bcd50384badf1449560282a8a59d4
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-28 15:10:04 +0530

    Add files via upload

--- Pattern48.java
+++ Pattern48.java
@@ -2,20 +2,23 @@ package patternapp;
 
 public class Pattern48 {
 	public static void main(String[] args) {
-		int x=5,y=1;
+		int x=5,y=1,z=0,k=4;
 		for(int i=1;i<=5;i++)
 		{
-		
+			z=y;
 			for(int j=1;j<=i;j++) {
 				if(i==j)
 					System.out.println(i);
 				else
-				System.out.print(y+" ");
+				System.out.print(z+" ");
 				
+				z=z-k-1;			
+						
+
 			}
 			y=y+x;
 			x--;
-			
+			k--;
 			System.out.println();
 
 		}
