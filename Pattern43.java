09ec05c07463f3fa26b449e12607d339c2325676
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-28 11:03:06 +0530

    Add files via upload

--- Pattern43.java
+++ Pattern43.java
@@ -3,14 +3,15 @@ package patternapp;
 public class Pattern43 {
 	public static void main(String[] args) {
 		int x=1;
+
 		for(int i=1;i<=5;i++)
 		{
-			
+			 x=i-1;
+
 			for(int j=1;j<=i;j++) {
-				System.out.print(x+" ");
+				System.out.print(x+i+" ");
 				x+=2;
 			}
-
 			System.out.println();
 
 		}
