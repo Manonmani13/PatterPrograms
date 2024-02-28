b4608a7dec40dec287a887ad0a87fba4c358acdf
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-28 17:32:28 +0530

    Add files via upload

---
+++ ConcatInSB.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class ConcatInSB {
+	public static void main(String[] args) {
+		StringBuffer s1=new StringBuffer("welcome to ");
+		s1.append("java");
+		System.out.println(s1);
+	}
+}
