35f5b2f3d926ca778a0eb61540a4113f7e481c2d
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-29 10:50:56 +0530

    Add files via upload

--- Pattern49.java
+++ Pattern49.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 		for(int j=1;j<=i;j++) {
 			if(i==j)
 			System.out.print(i+" ");
-			else if((i+j)%2==1) {
+			else if((i+j)%2==1 && (i!=4&& j!=1)|| (i!=5&& j!=2)) {
 				System.out.print(k+" ");
 				k--;
 			}
