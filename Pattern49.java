693f590a165a51aa518aaff4dcd9aafff44cf7a4
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-29 11:28:07 +0530

    Add files via upload

--- Pattern49.java
+++ Pattern49.java
@@ -2,19 +2,25 @@ package patternapp;
 
 public class Pattern49 {
 public static void main(String[] args) {
-	int k=9,l=10;
+	int k=9,l=10,m=14;
 	for(int i=1;i<=5;i++){
 		for(int j=1;j<=i;j++) {
 			if(i==j)
 			System.out.print(i+" ");
-			else if((i+j)%2==1 && (i!=4&& j!=1)|| (i!=5&& j!=2)) {
+			else if(i-j==1) {
 				System.out.print(k+" ");
 				k--;
 			}
-			else if((i+j)%2==0 ) {
+			else if(i-j==2 ) {
 				System.out.print(l+" ");
 				l--;
 			}
+			else if(i-j==3 ) {
+				System.out.print(m+" ");
+				m--;
+			}
+			else
+				System.out.print(15+" ");
 			
 		}
 		System.out.println();
