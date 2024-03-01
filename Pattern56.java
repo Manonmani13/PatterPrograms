d279009ff5dd464f7aabbdc927dae62e06a1d7c3
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-03-01 10:37:12 +0530

    Add files via upload

---
+++ Pattern56.java
@@ -0,0 +1,21 @@
+package patternapp;
+
+public class Pattern56 {
+public static void main(String[] args) {
+	int k=4;
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			if(j==1)
+				System.out.print(i+" ");
+			else {
+				System.out.print(i+k+" ");
+				k--;
+			}
+		}
+		System.out.println();
+		k=4;
+	}
+}
+}
